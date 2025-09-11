3530c44006a70cd35acb1a0af08e17ca8953c8a1
kiwon <kkw-11@naver.com>
2025-09-11 20:44:39 +0900

    test:Thymeleaf test

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -41,6 +41,24 @@ public class ThymeleafExController {
 
         model.addAttribute("itemDtoList",itemDtoList);
 
-        return "thymeleafEx/thymeleafEx03.html";
+        return "thymeleafEx/thymeleafEx03";
+    }
+
+
+    @GetMapping(value = "/ex05")
+    public String thymeleafExample05(){
+        return "thymeleafEx/thymeleafEx05";
+    }
+
+    @GetMapping(value = "/ex06")
+    public String thymeleafExample06(String param1, String param2, Model model){
+        model.addAttribute("param1",param1);
+        model.addAttribute("param2",param2);
+        return "thymeleafEx/thymeleafEx06";
+    }
+
+    @GetMapping("/ex07")
+    public String thymeleafExample07(){
+        return "thymeleafEx/thymeleafEx07";
     }
 }
