6956efa7588a71c36c4bc03237dc24f351b44736
kiwon <kkw-11@naver.com>
2025-09-13 13:45:40 +0900

    feat:Security 로그인, loadUserByUsername 메서드 구현

--- src/main/java/com/shop/service/MemberService.java
+++ src/main/java/com/shop/service/MemberService.java
@@ -3,13 +3,19 @@ package com.shop.service;
 import com.shop.entity.Member;
 import com.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+@Slf4j
+public class MemberService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     public Member saveMember(Member member){
@@ -22,5 +28,22 @@ public class MemberService {
         if(findMember != null){
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        log.info("로그인 시도 =====> email: {}", email);
+        Member member = memberRepository.findByEmail(email);
+        if(member == null){
+            log.info("===== 회원 데이터 없음 =====");
+            throw new UsernameNotFoundException(email);
+        }
+        log.info("===== 로그인 회원 조회 성공 =====");
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
     }
 }
