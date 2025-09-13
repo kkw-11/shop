192506bdd3bfb925c774d55955888677a917a873
kiwon <kkw-11@naver.com>
2025-09-13 13:46:11 +0900

    feat:로그인, 로그인 실패 컨트롤러 구현

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -42,4 +42,16 @@ public class MemberController {
 
         return "redirect:/";
     }
+
+    @GetMapping(value = "/login")
+    public String loginMember(Model model){
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        return "member/memberLoginForm";
+    }
+
+    @GetMapping("/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
+        return "member/memberLoginForm";
+    }
 }
