f65100a2bead67e86aaa466df6d17dfa565757b8
kiwon <kkw-11@naver.com>
2025-09-14 16:58:56 +0900

    feat:#5 Entity 공통 기능 속성화

---
+++ src/main/java/com/shop/entity/BaseTimeEntity.java
@@ -0,0 +1,27 @@
+package com.shop.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class BaseTimeEntity {
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime regTime;
+
+    @UpdateTimestamp
+    @Column(insertable = false)
+    private LocalDateTime updateTime;
+}
