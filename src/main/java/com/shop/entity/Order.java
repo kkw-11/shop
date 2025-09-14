cbcbf2ff935d3d8b747aadb015f67090ced6d4e1
kiwon <kkw-11@naver.com>
2025-09-14 10:34:31 +0900

    feat:Order, OrderItem Entity 설계

---
+++ src/main/java/com/shop/entity/Order.java
@@ -0,0 +1,33 @@
+package com.shop.entity;
+
+import com.shop.contant.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "order_id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime orderDate; //주문일
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; //주문상태
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+}
