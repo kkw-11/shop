160bd259f3d2bb3233308400614b80b960d1c593
kiwon <kkw-11@naver.com>
2025-09-15 19:39:03 +0900

    feat: #12 상품 수정 서비스 메서드

--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -1,19 +1,24 @@
 package com.shop.service;
 
 import com.shop.dto.ItemFormDto;
+import com.shop.dto.ItemImgDto;
 import com.shop.entity.Item;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
 import com.shop.repository.ItemRepository;
+import jakarta.persistence.EntityExistsException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
+@Slf4j
 public class ItemService {
     private final ItemRepository itemRepository;
     private final ItemImgService itemImgService;
@@ -40,4 +45,30 @@ public class ItemService {
         return item.getId();
     }
 
+    @Transactional(readOnly = true)
+    public ItemFormDto getItemDtl(Long itemId) {
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+        List<ItemImgDto> itemImgDtoList = itemImgList.stream().map(itemImg -> ItemImgDto.of(itemImg)).collect(Collectors.toList());
+
+        Item item = itemRepository.findById(itemId).orElseThrow(EntityExistsException::new);
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+        return itemFormDto;
+    }
+
+    public Long updateItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception {
+        //상품 수정
+        Item item = itemRepository.findById(itemFormDto.getId()).orElseThrow(EntityExistsException::new);
+        log.info("ItemService updateItem 상품 수정 전 {}", item.toString());
+        item.updateItem(itemFormDto);
+        log.info("ItemService updateItem 상품 수정 후 {}", item.toString());
+
+        List<Long> itemImgIds = itemFormDto.getItemImgIds();
+
+        for(int i =0; i < itemImgFileList.size(); i++){
+            itemImgService.updateItemImg(itemImgIds.get(i), itemImgFileList.get(i));
+        }
+
+        return item.getId();
+    }
 }
