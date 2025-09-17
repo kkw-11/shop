145877a86a4959a1011454cb88c8de7dd47c832c
kiwon <kkw-11@naver.com>
2025-09-17 12:59:07 +0900

    chore: #14 ItemSearchDto 필드명 설명 주석 작성

--- src/main/java/com/shop/dto/ItemSearchDto.java
+++ src/main/java/com/shop/dto/ItemSearchDto.java
@@ -4,10 +4,13 @@ import com.shop.constant.ItemSellStatus;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 검색 조건 DTO
+ */
 @Getter @Setter
 public class ItemSearchDto {
-    private String searchDateType;
-    private ItemSellStatus searchSellStatus;
-    private String searchBy;
-    private String searchQuery = "";
+    private String searchDateType; // 상품 등록일
+    private ItemSellStatus searchSellStatus; //상품 판매 상태
+    private String searchBy; // 상품 조회 검색 유형(상품명, 상품 등록자 아이디)
+    private String searchQuery = ""; // 조회할 검색어
 }
