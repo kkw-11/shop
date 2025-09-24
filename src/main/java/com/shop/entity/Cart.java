1db650e476fe4d06562b070c6cf2b96dfc6c5b11
kiwon <kkw-11@naver.com>
2025-09-24 17:25:59 +0900

    refactor: Cart pk column 수정

--- src/main/java/com/shop/entity/Cart.java
+++ src/main/java/com/shop/entity/Cart.java
@@ -12,7 +12,7 @@ import lombok.ToString;
 public class Cart extends BaseEntity {
 
     @Id
-    @Column
+    @Column(name = "cart_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
