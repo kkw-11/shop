a95de2e4b7990d85b19666e3ca47336aa0283ec6
kiwon <kkw-11@naver.com>
2025-09-27 13:54:27 +0900

    feat: #28 Swagger 적용 문서화, Cart, Order

--- src/main/java/com/shop/controller/CartController.java
+++ src/main/java/com/shop/controller/CartController.java
@@ -4,6 +4,10 @@ import com.shop.dto.CartDetailDto;
 import com.shop.dto.CartItemDto;
 import com.shop.dto.CartOrderDto;
 import com.shop.service.CartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,10 +26,17 @@ import java.util.List;
 @Slf4j
 @Controller
 @RequiredArgsConstructor
+@Tag(name = "장바구니 관리", description = "장바구니 관련 API")
 public class CartController {
     private final CartService cartService;
 
     @PostMapping(value = "/cart")
+    @Operation(summary = "장바구니 담기", description = "구매할 상품을 장바구니에 담기 요청.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청 성공"),
+            @ApiResponse(responseCode = "400", description = "인증 실패"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     public @ResponseBody ResponseEntity order(@RequestBody @Valid CartItemDto cartItemDto, BindingResult bindingResult, Principal principal){
         if (bindingResult.hasErrors()) {
             StringBuilder sb = new StringBuilder();
@@ -54,6 +65,12 @@ public class CartController {
     }
 
     @PatchMapping(value = "/cartItem/{cartItemId}")
+    @Operation(summary = "장바구니 상품 수량 업데이트", description = "장바구니에 담은 상품 수량 수정 요청.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청 성공"),
+            @ApiResponse(responseCode = "400", description = "인증 실패"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     public @ResponseBody ResponseEntity updateCartItem(@PathVariable("cartItemId") Long cartItemId, int count, Principal principal){
         if(count <= 0){
             return new ResponseEntity<String>("최소 1개 이상 담아주세요", HttpStatus.BAD_REQUEST);
@@ -66,6 +83,12 @@ public class CartController {
     }
 
     @DeleteMapping(value = "/cartItem/{cartItemId}")
+    @Operation(summary = "장바구니 상품 제거", description = "장바구니에 담은 상품 제거 요청.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청 성공"),
+            @ApiResponse(responseCode = "400", description = "인증 실패"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     public @ResponseBody ResponseEntity deleteCartItem(@PathVariable("cartItemId") Long cartItemId, Principal principal){
         if(!cartService.validateCartItem(cartItemId, principal.getName())){
             return new ResponseEntity<String>("수정 권한이 없습니다.", HttpStatus.FORBIDDEN);
@@ -75,6 +98,12 @@ public class CartController {
     }
 
     @PostMapping(value = "/cart/orders")
+    @Operation(summary = "장바구니 상품 주문", description = "장바구니에 담은 상품 주문 요청.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청 성공"),
+            @ApiResponse(responseCode = "400", description = "인증 실패"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     public @ResponseBody ResponseEntity orderCartItem(@RequestBody CartOrderDto cartOrderDto, Principal principal){
         List<CartOrderDto> cartOrderDtoList = cartOrderDto.getCartOrderDtoList();
 
