a40176efad6652fafd7381f32baccea98c54af07
kiwon <kkw-11@naver.com>
2025-10-05 15:37:20 +0900

    refactor: #36 장바구니 상품 생성 정적 팩토리 메서드

--- src/main/java/com/shop/entity/OrderItem.java
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -1,8 +1,13 @@
 package com.shop.entity;
 
+import com.shop.dto.OrderDto;
+import com.shop.repository.ItemRepository;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Map;
 
 @Entity
 @Table(name = "order_item")
@@ -32,6 +37,33 @@ public class OrderItem extends BaseEntity {
         orderItem.setOrderPrice(item.getPrice());
 
         item.removeStock(count);
+        return orderItem;
+    }
+
+    // 정적 팩토리 메서드 (Setter 대신 생성자 사용)
+    public static OrderItem createOrderItem(OrderDto orderDto, ItemRepository itemRepository) {
+        OrderItem orderItem = new OrderItem();
+        Item item = itemRepository.findById(orderDto.getItemId())
+                .orElseThrow(EntityNotFoundException::new);
+
+        orderItem.setItem(item);
+        orderItem.setCount(orderDto.getCount());
+        return orderItem;
+    }
+
+
+    public static OrderItem createOrderItem(OrderDto orderDto, Map<Long, Item> itemMap) {
+        OrderItem orderItem = new OrderItem();
+        // DB 호출 대신 Map에서 O(1)의 속도로 Item을 조회
+        Item item = itemMap.get(orderDto.getItemId());
+
+        if (item == null) {
+            // DTO에 포함된 Item ID가 DB에 없는 경우 처리 (필수 예외 처리)
+            throw new EntityNotFoundException("요청된 상품 ID " + orderDto.getItemId() + "를 찾을 수 없습니다.");
+        }
+
+        orderItem.setItem(item);
+        orderItem.setCount(orderDto.getCount());
         return orderItem;
     }
 
