03e0f9ff42793629fcefec4538c3aaf862b1e585
kiwon <kkw-11@naver.com>
2025-10-16 11:29:58 +0900

    refactor: #45 OrderRepository, QuerydslPredicateExecutor 제거

--- src/main/java/com/shop/repository/OrderRepository.java
+++ src/main/java/com/shop/repository/OrderRepository.java
@@ -4,5 +4,5 @@ import com.shop.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface OrderRepository extends JpaRepository<Order, Long>, QuerydslPredicateExecutor<Order>, OrderRepositoryCustom {
+public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
 }
