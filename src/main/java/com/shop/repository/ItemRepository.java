7e74907154f3c8d1ac04d7099516fca17c64d6eb
kiwon <kkw-11@naver.com>
2026-01-06 08:22:20 +0900

    fix: #53 상품조회 메서드 분리, Lock 사용, readOnly전용

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -4,10 +4,12 @@ import com.shop.entity.Item;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.repository.query.Param;
 
 public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item>, ItemRepositoryCustom {
     List<Item> findByItemNm(String itemNm);
@@ -18,7 +20,11 @@ public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredi
 
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
+
     @Override
     Optional<Item> findById(Long id);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT i FROM Item i WHERE i.id = :itemId")
+    Optional<Item> findByIdForUpdate(@Param("itemId") Long itemId);
 }
